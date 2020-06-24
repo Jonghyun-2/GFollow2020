3909ac356f38f6955425443c9f61cb0766ade39e
Jonghyun-2 <mickey9624@bme.gachon.ac.kr>
2020-06-24 20:37:31 +0900

    Diagnosis update

--- mysite/imgupload/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-def home(request):
-    return render(request, r"imgupload/home.html")
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
